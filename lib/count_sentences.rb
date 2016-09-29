b570240775c46ec5b501f7698be17e1330579d9e
Jessica Harrelson <jess.harrelson0707@gmail.com>
2016-09-29 15:55:10 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else self.end_with?("!")
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
-  end
+    if self.end_with?("!")
+          true
+        else
+          false
+        end
+      end
 
   def count_sentences
-
-  end
+    count = 0
+    self_array = self.split(" ")
+      self_array.each do |word|
+      if word.sentence?
+        count = count + 1
+      elsif word.question?
+        count = count + 1
+      elsif word.exclamation?
+        count = count +1
+      end
+    end
+  count
+end
 end
